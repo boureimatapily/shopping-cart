6a263463d3d1a739fba6bc3dd95af617fde4e185
Boureima Tapily <boureimatapily1@gmail.com>
2019-11-19 15:00:37 +0000

    MainContent renders ProductNumber.js, OrderBy.js and Stock.js

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Stcok from './Stock';
+import OrderBy from './OrderBy';
+import ProductNumber from './ProductNumber';
+
 
 const MainContent = () => {
     return (
-        <div>
-            <h1>This is MainContent Component</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-6">
+                    <ProductNumber />
+                </div>
+                <div className="col-lg-6">
+                    <OrderBy />
+                </div>
+            </div>
+            <div className="row">
+                <Stcok />
+            </div>
+            
         </div>
     )
 }
