2487987189c7a87570196dc566d6802644da03aa
Boureima Tapily <boureimatapily1@gmail.com>
2019-11-19 14:06:29 +0000

    boilerplate for components

---
+++ src/components/ShopPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ShopPage = () => {
+    return (
+        <div>
+            <h1> This is ShopPage Component</h1>
+        </div>
+    )
+}
+export default ShopPage;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div>
+            <h1> This is Sidebar Component</h1>
+        </div>
+    )
+}
+export default Sidebar;
